321ceedd85563c79fb8c070163a996876c133ad5
Utsav Shah <utsavahsesh.shah@student.griffith.ie>
2020-05-03 17:41:50 +0100

    Bracket 6 Wokring
    All functionalites working

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from post import Post
 from uploadhandler import UploadHandler
 from user_profile import UserProfile
 from list import List
+from comments import Comments
 
 JINJA_ENVIRONMENT = jinja2.Environment(
 loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -46,20 +47,20 @@ class MainPage(webapp2.RequestHandler):
             myuser = MyUser(id=user.user_id(), email_address=email_address, username = name[0].lower(), followers_count = 0, following_count = 0)
             myuser.put()
 
+        img_url_list = []
+        no_posts = False
 
         if myuser.following != []:
             query = Post.query().filter(ndb.OR(Post.post_by.IN(myuser.following),Post.post_by==myuser_key)).order(-Post.upload_time).fetch(50)
         else:
             query = Post.query().filter(Post.post_by==myuser_key).order(-Post.upload_time).fetch(50)
 
-        img_url_list = []
-        caption_list = []
-        username_list = []
-        for i in query:
-            username_list.append(i.post_by.get())
-            caption_list.append(i.caption)
-            url = images.get_serving_url(i.uploads)
-            img_url_list.append(url)
+        if query != []:
+            for i in query:
+                url = images.get_serving_url(i.uploads)
+                img_url_list.append(url)
+        else:
+            no_posts = True
 
         #With the details, we render the Home page of our application
         template_values = {
@@ -67,13 +68,30 @@ class MainPage(webapp2.RequestHandler):
             'user':user,
             'myuser': myuser,
             'img_url_list': img_url_list,
-            'username_list': username_list,
-            'caption_list':caption_list,
+            'query': query,
+            'no_posts': no_posts,
             'upload_url' : blobstore.create_upload_url('/upload')
         }
 
         template = JINJA_ENVIRONMENT.get_template('main.html')
         self.response.write(template.render(template_values))
+
+    def post(self):
+
+        user = users.get_current_user()
+
+        myuser_key = ndb.Key('MyUser', user.user_id())
+        myuser = myuser_key.get()
+
+        comment = self.request.get('comment')
+        post_id = self.request.get('query')
+        post_key = ndb.Key(Post, long(post_id))
+        post = post_key.get()
+        new_comment = Comments(comment = comment, comment_by = myuser_key)
+        post.comments.append(new_comment)
+        post.put()
+        self.redirect('/')
+
 
 app = webapp2.WSGIApplication([
         ('/',MainPage),
